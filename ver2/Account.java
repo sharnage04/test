6e229a8b734ad9a5c24a59fec13befce5746d9fc
sharnage04 <sharnage@valdosta.edu>
2024-08-25 21:22:38 -0400

    constructor delete

--- ver2/Account.java
+++ ver2/Account.java
@@ -8,11 +8,6 @@ public class Account {
 	
 	private String name;
 	
-	// Constructor
-	public Account(double balance) {
-		this("Unknown" , balance);
-	}
-	
 	public Account(String name, double balance) {
 		this.name = name;
 		this.balance = balance;
